37896b3bc169bc0f53d9fc362a276c08e9fef2fd
noismik <www_lol@naver.com>
2023-07-19 21:53:56 +0900

    숙제 최종

--- src/main/java/com/mycompany/springhomework/controller/HomeController.java
+++ src/main/java/com/mycompany/springhomework/controller/HomeController.java
@@ -8,12 +8,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class HomeController {
+	public HomeController() {
+		log.info("실행");
+	}
 	//http://localhost:8080/springwebapp
 	@RequestMapping("/")
 	public String index() {
 		
 		log.info("실행1");
-		return "index";
+		return "home";
 		
 		
 	}
---
+++ src/main/java/com/mycompany/springhomework/dto/Ch02FileInfo.java
@@ -0,0 +1,11 @@
+package com.mycompany.springhomework.dto;
+
+import lombok.Data;
+
+@Data
+public class Ch02FileInfo {
+	private String fileName;
+
+	
+
+}

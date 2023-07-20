7c1a720936edb35cae8541d0152505c5ad68b645
noismik <www_lol@naver.com>
2023-07-20 14:01:07 +0900

    홈월크

--- src/main/java/com/mycompany/springhomework/controller/Ch02Controller.java
+++ src/main/java/com/mycompany/springhomework/controller/Ch02Controller.java
@@ -1,7 +1,13 @@
 package com.mycompany.springhomework.controller;
 
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
@@ -13,8 +19,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.mycompany.springhomework.dto.Ch02Dto;
 import com.mycompany.springhomework.dto.Ch02FileInfo;
+import com.mycompany.springhomework.interceptor.Auth;
+import com.mycompany.springhomework.interceptor.Auth.Role;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,6 +38,16 @@ public class Ch02Controller {
       
       return "ch02/content";
    }
+   
+  /* 위 표현과 밑 표현이 같음 
+	
+	  @RequestMapping("/content")
+	   public ModelAndView content() {
+	      ModelAndView mav = new ModelAndView();
+	      mav.setViewName("ch02/content");
+	      mav.addObject("data", "홍길동");
+	      return mav;
+	   }*/
    
    @RequestMapping(value="/method", method=RequestMethod.GET)
    public String method1(String bkind, int bno) {
@@ -45,7 +65,7 @@ public class Ch02Controller {
       return "ch02/content";
    }
    
-   @PutMapping("/method")
+/*   @PutMapping("/method")
    public void method3(@RequestBody String json, HttpServletResponse response) throws Exception {
       JSONObject jsonObject = new JSONObject(json);
       String bkind = jsonObject.getString("bkind");
@@ -63,6 +83,24 @@ public class Ch02Controller {
       pw.flush();
       pw.close();
       
+   }*/
+   
+   
+   @PutMapping("/method")
+   public void method3(@RequestBody Ch02Dto dto, HttpServletResponse response) throws Exception {
+	   log.info("bkind: " + dto.getBkind());
+	   log.info("bno: " + dto.getBno());
+	   
+	   JSONObject root = new JSONObject();
+	   root.put("result", "success");
+	   String responseJson = root.toString();
+	   
+	   response.setContentType("application/json; charset=UTF-8");
+	   PrintWriter pw = response.getWriter();
+	   pw.print(responseJson);
+	   pw.flush();
+	   pw.close();
+	   
    }
    
    @DeleteMapping("/method")
@@ -120,6 +158,55 @@ public class Ch02Controller {
       
       return fileInfo;
    }
+   
+   @GetMapping("/fileDownload")
+   public void fileDownload(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	    String fileName = "photo1.jpg";
+		String filePath = "/resources/image/photo/" + fileName;
+		filePath = request.getServletContext().getRealPath(filePath);
+		log.info(filePath);
+		
+		//InputStream is = new FileInputStream(filePath);
+		//BufferedInputStream bis = new BufferedInputStream(is);
+		
+		//응답 헤드에 Content-Type 추가
+		String mimeType = request.getServletContext().getMimeType(filePath);
+		response.setContentType(mimeType); //image/jpeg, image/png
+		
+		//응답 헤드에 한글 이름의 파일명을 ISO-8859-1 문자셋으로 인코딩해서 추가
+		String userAgent = request.getHeader("User-Agent");
+		if(userAgent.contains("Trident") || userAgent.contains("MSIE")) {
+				//IE
+			fileName = URLEncoder.encode(fileName, "UTF-8");
+			log.info("IE: " + fileName);
+			
+		} else {
+			//Chrome, Edge, FireFox, Safari
+			fileName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+			log.info("Chrome: " + fileName);
+		}
+		response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+		
+		//응답 본문에 파일 데이터 싣기
+		OutputStream os = response.getOutputStream();
+		Path path = Paths.get(filePath);
+		Files.copy(path, os);
+		os.flush();
+		os.close();
+	   
+	   
+   }
+   
+   //value는 생략 가능
+   //조은 방법! 권한별로 페이지 설정가능.
+   @RequestMapping("/filterAndInterceptor")
+   @Auth(Role.ADMIN)
+   public String adminMethod() {
+	   log.info("실행");
+	   return "ch02/adminPage";
+	   
+   }
+   
    
    
 }
